a333309ce5f391dbe8b13b09ffbcdb0347273393
Michaela Chang <michaela.w.chang@gmail.com>
2022-12-22 14:04:40 -0800

    SCSS with sveltekit hopefully working

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<slot />
+
+<style lang="scss">
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
+    :global(body) {
+        font-family: 'Inter', sans-serif;
+    }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		}),
 		paths: {
 			base: '',
-		},
+		}
 	}
 };
 
