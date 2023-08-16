0e9cee956119e6cf962d3dd8ea7af26b04051d58
Michaela Chang <michaela.w.chang@gmail.com>
2023-08-16 12:49:47 -0700

    Fixed issue where getting date caption from filename doesn't work with resolver url.

--- src/routes/(app)/about/+page.js
+++ src/routes/(app)/about/+page.js
@@ -9,7 +9,7 @@ export async function load() {
     const pfps = await Promise.all(
         iterableModule.map(async([path, resolver]) => {
             const pfpData = await resolver().then(({default: imageUrl}) => {
-                let date = new Date(imageUrl.substring(0, imageUrl.length - 3));
+                let date = new Date(path.substring(0, path.length - 3));
                 let dateVerbose = date.toLocaleDateString("en-US", options);
                 return { 
                     url: imageUrl, 
