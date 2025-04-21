11aaa10d041180082ddfb6e8f1bc9309d54d50da
Michaela Chang <michaela.w.chang@gmail.com>
2025-04-20 17:23:46 -0700

    Add landing page info, change build target folder to docs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,8 @@
     import { base } from "$app/paths";
 </script>
 
-Hello World!
+<svelte:head>
+    <title>Milkcee Studios</title>
+</svelte:head>
+
+<h1>Hello World! This site is currently under construction. To see the old site, click <a href="https://milkcee12.github.io/v1-skim">here</a>.</h1>
